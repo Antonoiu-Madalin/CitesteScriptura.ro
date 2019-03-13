867c6475fc33db6439e79d42a13386157c31a187
Antonoiu <Madalin.Antonoiu@merz.com>
2019-03-13 06:47:41 -0400

    Added Logo text !!! yay

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -44,10 +44,21 @@ export class Navigation extends Component {
 
     return (
       <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
+ 
         <div className="Nav--Container container">
-          <Link to="/" onClick={this.handleLinkClick}>
+          
+       
+          <Link to="/" className="logoMover" onClick={this.handleLinkClick}>
             <Logo />
           </Link>
+
+          <Link to="/" onClick={this.handleLinkClick}>
+            <a href="/" className="logoTextA">
+              Citește Scriptura
+            </a>
+          </Link>
+          
+
 
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
