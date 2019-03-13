7d3fec37da7bd77588ac46acfb667e2911127c86
Antonoiu <Madalin.Antonoiu@merz.com>
2019-03-13 07:28:10 -0400

    ContactPage.js & .css - same as cs.n.com

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { MapPin, Smartphone, Mail } from 'react-feather'
+import { Mail } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
 import FormSimpleAjax from '../components/FormSimpleAjax'
 import Content from '../components/Content'
-import GoogleMap from '../components/GoogleMap'
 import Layout from '../components/Layout'
 import './ContactPage.css'
 
@@ -15,10 +14,7 @@ export const ContactPageTemplate = ({
   title,
   subtitle,
   featuredImage,
-  address,
-  phone,
   email,
-  locations
 }) => (
   <main className="Contact">
     <PageHeader
@@ -31,23 +27,7 @@ export const ContactPageTemplate = ({
         <div>
           <Content source={body} />
           <div className="Contact--Details">
-            {address && (
-              <a
-                className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
-                  address
-                )}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <MapPin /> {address}
-              </a>
-            )}
-            {phone && (
-              <a className="Contact--Details--Item" href={`tel:${phone}`}>
-                <Smartphone /> {phone}
-              </a>
-            )}
+
             {email && (
               <a className="Contact--Details--Item" href={`mailto:${email}`}>
                 <Mail /> {email}
@@ -61,8 +41,6 @@ export const ContactPageTemplate = ({
         </div>
       </div>
     </section>
-
-    <GoogleMap locations={locations} />
   </main>
 )
 
