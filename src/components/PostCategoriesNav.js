da1510f800c519d7779bcfe965a6ac6d9f26108d
Antonoiu <Madalin.Antonoiu@merz.com>
2019-05-03 08:04:29 -0400

    Does this work?

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Collapsible from './Collapsible';
 import './PostCategoriesNav.css'
+import { FaTags } from 'react-icons/fa';
 import BlogSearch from './BlogSearch';
 
 
@@ -15,18 +17,18 @@ const PostCategoriesNav = ({ categories}) => (
   <div id="collapsibleContainer">
   
   {/*The proper search! Do not remove */}
-    
+    <div className="properSearch"> 
       <BlogSearch />
-   
+    </div>
 
-
-   
-   
+    <div id="innerCollapsible">
+    <Collapsible trigger={<FaTags className="zeIcon"/>} className="collapSible" >
+    
       <div className="PostCategoriesNav">
         <Link className="NavLink" exact="true" to={`/`} activeStyle={activeStyles}>
           Toate
         </Link>
-    
+
         {categories.map((category, index) => (
           <Link
             exact="true"
@@ -40,8 +42,8 @@ const PostCategoriesNav = ({ categories}) => (
         ))}
 
       </div>
-
-    
+      </Collapsible>
+      </div>
     </div>
 )
 
