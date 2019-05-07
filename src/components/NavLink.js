c946ac44150ebcf6f97951f6cf5ad84fdbbf6540
Antonoiu <Madalin.Antonoiu@merz.com>
2019-05-07 04:49:03 -0400

    Should fix the navLink display

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import './NavLink.css'
 
 export default ({ className, children, ...props }) => (
   <Link {...props} className={`NavLink ${className || ''}`}>
