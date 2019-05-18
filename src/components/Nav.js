f65d530034a20583cec0dc30c88d9a48d4909cd6
Antonoiu <Madalin.Antonoiu@merz.com>
2019-05-17 23:02:10 -0400

    ⭐ Beta-Release 1.5.0

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -61,7 +61,7 @@ export class Navigation extends Component {
           </div>
 
           <div  className="logoTextA">
-            <p className="noselect">BetaRelease 1.1.0</p>         
+            <p className="noselect">BetaRelease 1.5.0</p>         
           </div>
          
           <div className="Nav--Links noselect">
