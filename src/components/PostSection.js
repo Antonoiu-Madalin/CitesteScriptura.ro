e80f57d0280e07d821edf88c5b4092d2af90388b
@mantonoiu <antonoiumadalin@gmail.com>
2019-07-09 19:21:34 +0000

    minor playaround

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -53,3 +53,56 @@ class PostSection extends React.Component {
 }
 
 export default PostSection
+
+/*import React,{ useState } from 'react'
+import PostCard from '../components/PostCard'
+import './PostSection.css'
+
+export default function PostSection (props) {
+
+  const {
+    posts= [], // here im putting all posts inside an array
+    title= '',
+    limit= 12, //This does work, changes how many posts appear per page
+    showLoadMore= true,
+    className= 'ButtonulMeu',
+    loadMoreTitle= 'Mai multe articole',
+    perPageLimit= 11,
+
+    } = props;
+
+// This is the part i need help with
+const increaseLimit = () => {
+   const listState = useState( limit );
+   listState(prevState => ({limit: prevState.limit + perPageLimit}))
+} // This is the part i need help with
+
+
+const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to display only limit array Posts
+
+
+    return (
+
+      <div className="PostSection">
+
+        {title && <h2 className="PostSection--Title">{title}</h2>}
+        {!!visiblePosts.length && (
+          <div className="PostSection--Grid">
+            {visiblePosts.map((post, index) => (
+              <PostCard key={post.title + index} {...post} />
+            ))}
+          </div>
+        )}
+        {showLoadMore && visiblePosts.length < posts.length && (
+          <div className="taCenter">
+            <button className="buttonulMorePosts" onClick={increaseLimit/*So when i click this i want to show few more posts on page}>
+              {loadMoreTitle}
+            </button>
+          </div>
+        )}
+        </div>
+
+    )
+  }
+
+*/
