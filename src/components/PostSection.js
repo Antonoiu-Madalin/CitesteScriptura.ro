7c5c03828ab7ea77755bb70ba1d6db2e155d445e
@mantonoiu <antonoiumadalin@gmail.com>
2019-07-09 20:24:27 +0000

    t2ca from Discord fixed it - Hook Compatible now~

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -1,84 +1,20 @@
-import React from 'react'
-import PostCard from '../components/PostCard'
-import './PostSection.css'
-
-class PostSection extends React.Component {
-  static defaultProps = {
-    posts: [],
-    title: '',
-    limit: 12,
-    showLoadMore: true,
-    className: 'ButtonulMeu',
-    loadMoreTitle: 'Mai multe articole',
-    perPageLimit: 12
-  }
-
-  state = {
-    limit: this.props.limit
-  }
-
-  increaseLimit = () =>
-    this.setState(prevState => ({
-      limit: prevState.limit + this.props.perPageLimit
-    }))
-
-  render() {
-    const { posts, title, showLoadMore, loadMoreTitle } = this.props,
-      { limit } = this.state,
-      visiblePosts = posts.slice(0, limit || posts.length)
-
-    return (
-      
-      <div className="PostSection">
-      
-        {title && <h2 className="PostSection--Title">{title}</h2>}
-        {!!visiblePosts.length && (
-          <div className="PostSection--Grid">
-            {visiblePosts.map((post, index) => (
-              <PostCard key={post.title + index} {...post} />
-            ))}
-          </div>
-        )}
-        {showLoadMore && visiblePosts.length < posts.length && (
-          <div className="taCenter">
-            <button className="buttonulMorePosts" onClick={this.increaseLimit}>
-              {loadMoreTitle}
-            </button>
-          </div>
-        )}
-        </div>
-      
-    )
-  }
-}
-
-export default PostSection
-
-/*import React,{ useState } from 'react'
+import React,{ useState } from 'react'
 import PostCard from '../components/PostCard'
 import './PostSection.css'
 
 export default function PostSection (props) {
 
-  const {
-    posts= [], // here im putting all posts inside an array
-    title= '',
-    limit= 12, //This does work, changes how many posts appear per page
-    showLoadMore= true,
-    className= 'ButtonulMeu',
-    loadMoreTitle= 'Mai multe articole',
-    perPageLimit= 11,
-
-    } = props;
-
-// This is the part i need help with
-const increaseLimit = () => {
-   const listState = useState( limit );
-   listState(prevState => ({limit: prevState.limit + perPageLimit}))
-} // This is the part i need help with
+    const {
+        posts= [], // here im putting all posts inside an array
+        title= '',
+        showLoadMore= true,
+        loadMoreTitle= 'Mai multe articole',
+        } = props;
 
 
-const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to display only limit array Posts
+    const [limit, setLimit] = useState(12);
+
+    const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to display only limit array Posts
 
 
     return (
@@ -95,9 +31,10 @@ const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to displa
         )}
         {showLoadMore && visiblePosts.length < posts.length && (
           <div className="taCenter">
-            <button className="buttonulMorePosts" onClick={increaseLimit/*So when i click this i want to show few more posts on page}>
+            <button className="buttonulMorePosts" onClick={() =>{setLimit(limit+6)}}>
               {loadMoreTitle}
             </button>
+
           </div>
         )}
         </div>
@@ -105,4 +42,4 @@ const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to displa
     )
   }
 
-*/
+
