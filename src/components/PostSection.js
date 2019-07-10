dfff4b9bda5d0e19615c6e0dc46dd6fb495e3936
@mantonoiu <antonoiumadalin@gmail.com>
2019-07-10 08:33:00 +0000

    Arranged PostSection to not look so messy

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -2,43 +2,23 @@ import React,{ useState } from 'react'
 import PostCard from '../components/PostCard'
 import './PostSection.css'
 
+
 export default function PostSection (props) {
 
-    const {
-        posts= [], // here im putting all posts inside an array
-        title= '',
-        showLoadMore= true,
-        loadMoreTitle= 'Mai multe articole',
-        } = props;
-
-
+    const { posts= [], title= '', showLoadMore= true, loadMoreTitle= 'Mai multe articole',} = props;
     const [limit, setLimit] = useState(12);
-
-    const  visiblePosts  = posts.slice(0, limit || posts.length) //slicing to display only limit array Posts
-
+    const  visiblePosts  = posts.slice(0, limit || posts.length)
 
     return (
 
       <div className="PostSection">
-
-        {title && <h2 className="PostSection--Title">{title}</h2>}
-        {!!visiblePosts.length && (
-          <div className="PostSection--Grid">
-            {visiblePosts.map((post, index) => (
-              <PostCard key={post.title + index} {...post} />
-            ))}
-          </div>
-        )}
-        {showLoadMore && visiblePosts.length < posts.length && (
-          <div className="taCenter">
-            <button className="buttonulMorePosts" onClick={() =>{setLimit(limit+6)}}>
-              {loadMoreTitle}
-            </button>
-
-          </div>
-        )}
-        </div>
-
+            {/*1*/}
+            {title && <h2>{title}</h2>}
+            {/*2*/}
+            {!!visiblePosts.length && (   <div className="PostSection--Grid"> {visiblePosts.map((post, index) => (<PostCard key={post.title + index} {...post} />))} </div>)}
+            {/*3*/}
+            {showLoadMore && visiblePosts.length < posts.length && ( <div className="taCenter"> <button className="buttonulMorePosts" onClick={() =>{setLimit(limit+6)}}> {loadMoreTitle}</button></div>)}
+      </div>
     )
   }
 
